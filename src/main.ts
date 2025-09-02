a30868fd22824d2cb25dd1d7a0f0fda03b5363e7
pedrolopeshls99@gmail.com <pedrolopeshls99@gmail.com>
2025-09-02 16:28:12 -0300

    redefine vue project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,14 @@
-/**
- * main.ts
- *
- * Bootstraps Vuetify and other plugins then mounts the App`
- */
+import './assets/main.css'
 
-// Plugins
-import { registerPlugins } from '@/plugins'
-
-// Components
-import App from './App.vue'
-
-// Composables
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-// Styles
-import 'unfonts.css'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
 
-registerPlugins(app)
+app.use(createPinia())
+app.use(router)
 
 app.mount('#app')
